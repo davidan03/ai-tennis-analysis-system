38b9f2187ab452caacd32ebc1db9a96a9fff2d72
David An <totallydavidahn@gmail.com>
2025-11-27 17:14:07 -0800

    Test tennis ball detection model

--- inference.py
+++ inference.py
@@ -1,8 +1,8 @@
 from ultralytics import YOLO
 
-model = YOLO("yolo11x.pt")
+model = YOLO("./training/best.pt")
 
-result = model.predict("./inputs/input_video.mp4",
+result = model.track("./inputs/input_video.mp4",
                        save=True,
                        project="/Users/davidan/ai-tennis-analysis-system/ai-tennis-analysis-system/runs/detect")
 
