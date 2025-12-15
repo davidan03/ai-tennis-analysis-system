abd6279a240258a8540532a2b9cf77975e048f3b
David An <totallydavidahn@gmail.com>
2025-12-15 15:06:27 -0800

    Create filtering functionality for the two actual players

--- trackers/player_tracker.py
+++ trackers/player_tracker.py
@@ -1,10 +1,72 @@
 from ultralytics import YOLO
 import cv2
 import pickle
+from utils import get_center_of_box, distance_between_points
 
 class PlayerTracker:
     def __init__(self, model_path):
         self.model = YOLO(model_path)
+
+    def filter_players(self, court_keypoints, player_detections):
+        """
+        Returns a list of dictionaries of player IDs to bounding box coordinates
+        of the two actual players. Umpire, line judges, and ball children were
+        incorrectly identified as players, making this filtering necessary.
+        
+        :param court_keypoints: list of court keypoint coordinates
+        :param player_detections: list of dictionaries of player IDs to bounding
+                                  box coordinates (x min, y min) -> (x max, y max)
+        """
+        # Takes the first dictionary, equating to the detections from the first
+        # video frame and makes a call to the helper method
+        player_detections_first_frame = player_detections[0]
+        filtered_players = self.filter_players_helper(court_keypoints, player_detections_first_frame)
+        filtered_player_detections = []
+
+        # Uses list comprehension to filter for only the dictionaries of
+        # the actual players
+        for player_dict in player_detections:
+            filtered_player_dict = {id: bounding_box for id, bounding_box in player_dict.items() if id in filtered_players}
+            filtered_player_detections.append(filtered_player_dict)
+
+        return filtered_player_detections
+    
+    def filter_players_helper(self, court_keypoints, player_detection):
+        """
+        Generates a list of player IDs to their closest distance to a court keypoint.
+        Returns a list of IDs of the two closest players to the court, based on the
+        given keypoints.
+        
+        :param court_keypoints: list of court keypoint coordinates
+        :param player_detection: dictionary of player IDs to bounding box coordinates
+                                 (x min, y min) -> (x max, y max)
+        """
+        distances = []
+
+        # player_detection is a dictionary representing one video frame's worth of detections
+        # Maps player IDs to their bounding box coordinates
+        for id, bounding_box in player_detection.items():
+            player_center = get_center_of_box(bounding_box)
+
+            min_distance = float("inf")
+
+            for i in range(0, len(court_keypoints), 2):
+                court_keypoint = (court_keypoints[i], court_keypoints[i + 1])
+                distance = distance_between_points(court_keypoint, player_center)
+
+                # Determines the closest distance between the player and the court
+                if distance < min_distance:
+                    min_distance = distance
+            
+            # Appends the closest distance
+            distances.append((id, min_distance))
+
+        # Sorts the (ID, min_distance) tuples by ascending min_distance
+        # Filters the IDs of the two players with the closest distance to the court
+        distances.sort(key=lambda x: x[1])
+        filtered_players = [distances[0][0], distances[1][0]]
+        
+        return filtered_players
 
     def detect_frames(self, frames, read_from_stub=False, stub_path=None):
         """
@@ -40,7 +102,6 @@ class PlayerTracker:
 
         :param frame: a NumPy array representing a single video frame
         """
-
         # Need to index 0 because track() returns a list of Result objects, one per input image
         results = self.model.track(frame, persist=True)[0]
 
@@ -69,7 +130,6 @@ class PlayerTracker:
         :param video_frames: list of NumPy arrays representing video frames
         :param player_detections: list of dictionaries mapping player IDs to bounding box coordinates
         """
-        
         output_video_frames = []
 
         for frame, player_dict in zip(video_frames, player_detections):
