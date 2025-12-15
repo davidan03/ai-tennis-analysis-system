0d9259f266d693b3ee95ce98076b4e4e18663060
David An <totallydavidahn@gmail.com>
2025-12-15 15:04:57 -0800

    Add frames to top left of output and filter players

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from utils import read_video, save_video
 from trackers import PlayerTracker, BallTracker
 from court_line_detector import CourtLineDetector
+import cv2
 
 def main():
     # Declare video path and read video frames
@@ -20,21 +21,33 @@ def main():
     # Create CourtLineDetector object using the trained CNN
     court_line_detector = CourtLineDetector("models/keypoints_model.pth")
 
+    # Retrieve list of dictionaries of player IDs to bounding box coordinates
     player_detections = player_tracker.detect_frames(video_frames,
                                                     read_from_stub=True,
                                                      stub_path="tracker_stubs/player_detections.pkl")
+    
+    # Retrieve list of dictionaries of the ball's ID to bounding box coordinates
     ball_detections = ball_tracker.detect_frames(video_frames,
                                                  read_from_stub=True,
                                                  stub_path="tracker_stubs/ball_detections.pkl")
+    
+    # Interpolate ball positions where detections don't occur
     ball_detections = ball_tracker.interpolate_ball_positions(ball_detections)
+
+    # Predict court keypoints and draw them onto the video frames
+    court_keypoints = court_line_detector.predict(video_frames[0])
+    output_video_frames = court_line_detector.draw_keypoints_on_video(video_frames, court_keypoints)
+
+    # Filter for only the two actual players
+    player_detections = player_tracker.filter_players(court_keypoints, player_detections)
 
     # Draw bounding boxes on the video frames
     output_video_frames = player_tracker.draw_bounding_boxes(video_frames, player_detections)
     output_video_frames = ball_tracker.draw_bounding_boxes(video_frames, ball_detections)
 
-    # Predict court keypoints and draw them onto the video frames
-    court_keypoints = court_line_detector.predict(video_frames[0])
-    output_video_frames = court_line_detector.draw_keypoints_on_video(video_frames, court_keypoints)
+    # Write frame number in top left corner for each frame
+    for i, frame in enumerate(output_video_frames):
+        cv2.putText(frame, f"Frame: {i}", (10, 30), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 255, 0), 2)
 
     output_video_path = "outputs/output_video.avi"
     save_video(output_video_frames, output_video_path)
