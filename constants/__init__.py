b6ecbec7285671c7d2df5b5fc58fc260f4b15960
David An <totallydavidahn@gmail.com>
2025-12-15 19:56:34 -0800

    Create constants for tennis court measurements

---
+++ constants/__init__.py
@@ -0,0 +1,12 @@
+# All constants in meters (f the imperial system)
+SINGLES_LINE_WIDTH = 8.23
+DOUBLES_LINE_WIDTH = 10.97
+HALF_COURT_LENGTH = 11.88
+SERVICE_LINE_LENGTH = 6.40
+DOUBLES_ALLEY_DIFF = 1.37
+NO_MANS_LAND_WIDTH = 5.48
+
+# Heights are based on the players in the input video
+# Need to come up with better determination later
+PLAYER_1_HEIGHT = 1.88 # Djokovic's height
+PLAYER_2_HEIGHT = 1.91 # Sonego's height
