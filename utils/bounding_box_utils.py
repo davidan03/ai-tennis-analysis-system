e903fb4eb6c9e264ec6c06947ec421d6b6c1fb5f
David An <totallydavidahn@gmail.com>
2025-12-16 17:44:11 -0800

    Create helper methods for mini court

--- utils/bounding_box_utils.py
+++ utils/bounding_box_utils.py
@@ -14,3 +14,31 @@ def distance_between_points(p1, p2):
     distance = math.sqrt(math.pow(x2 - x1, 2) + math.pow(y2 - y1, 2))
 
     return distance
+
+def measure_xy_distance(p1, p2):
+    return abs(p1[0] - p2[0]), abs(p1[1] - p2[1])
+
+def get_foot_position(player_bounding_box):
+    x1, y1, x2, y2 = player_bounding_box
+
+    return (int((x1 + x2) / 2), y2)
+
+def get_closest_keypoint_index(point, court_keypoints, keypoint_indices):
+    closest_distance = float("inf")
+
+    # Candidate for closest keypoint index
+    keypoint_index = keypoint_indices[0]
+
+    for index in keypoint_indices:
+        # Retrieve x and y coordinates
+        keypoint = court_keypoints[index * 2], court_keypoints[index * 2 + 1]
+        distance = abs(point[1] - keypoint[1])
+
+        if distance < closest_distance:
+            closest_distance = distance
+            keypoint_index = index
+
+    return keypoint_index
+
+def get_bounding_box_height(bounding_box):
+    return bounding_box[3] - bounding_box[1]
